5b90ad880982a9db6ea4d84da24c20ba9002b0e1
Jeff Hammel <jhammel@mozilla.com>
2012-01-05 12:52:36 -0800

    Bug 715188 - refine Talos installation workflow;r=jmaher

--- INSTALL.py
+++ INSTALL.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 
 """
-installation script for talos
-
-You can run this script like:
-
-python <(curl http://hg.mozilla.org/build/talos/raw-file/tip/INSTALL.py)
-
-to get a talos virtualenv with talos in the `src` directory
+installation script for talos. This script:
+- creates a virtualenv in the current directory
+- sets up talos in development mode: `python setup.py develop`
+- downloads pageloader and packages to talos/page_load_test/pageloader.xpi
 """
 
 import os
 import re
 import shutil
-import string
 import subprocess
 import sys
 import urllib2
@@ -24,10 +20,27 @@ try:
 except:
     from subprocess import call
 
-REPO='http://hg.mozilla.org/build/talos'
-ZIPFILE='http://hg.mozilla.org/build/talos/archive/tip.zip'
-DEST='talos-env'
+# globals
+here = os.path.dirname(os.path.abspath(__file__))
+pageloader = 'http://hg.mozilla.org/build/pageloader/archive/tip.zip'
 VIRTUALENV='https://raw.github.com/pypa/virtualenv/develop/virtualenv.py'
+
+def get_pageloader(dest):
+    """
+    downloads pageloader and packages as an .xpi
+    """
+    output = os.path.join(dest, 'page_load_test', 'pageloader.xpi')
+    output = zipfile.ZipFile(dest, mode='w')
+    buffer = StringIO() # unfortunately, urllib2 "file like objects" do not have seek
+    buffer.write(urllib2.urlopen(pageloader).read())
+    input = zipfile.ZipFile(buffer)
+    for member in input.infolist():
+        contents = input.read(member)
+        member.filename = member.filename.split(os.path.sep, 1)[-1]
+        if member.filename == '.hg_archival.txt':
+            continue # ignore the hg generated file
+        output.writestr(member, contents)
+    output.close()
 
 def which(binary, path=os.environ['PATH']):
     dirs = path.split(os.pathsep)
@@ -39,43 +52,25 @@ def which(binary, path=os.environ['PATH']):
 
 def main(args=sys.argv[1:]):
 
-    # wipe vestiges
-    if os.path.exists(DEST):
-       shutil.rmtree(DEST) 
+    # sanity check
+    # ensure setup.py exists
+    setup_py = os.path.join(here, 'setup.py')
+    assert os.path.exists(setup_py), "setup.py not found"
+    # ensure page_load_test exists and is a directory
+    page_load_test = os.path.join(here, 'talos', 'page_load_test')
+    assert os.path.exists(page_load_test) and os.path.isdir(page_load_test), "Missing page_load_test directory"
 
     # create a virtualenv
     virtualenv = which('virtualenv') or which('virtualenv.py')
     if virtualenv:
-        call([virtualenv, '--system-site-packages', DEST])
+        call([virtualenv, '--system-site-packages', here])
     else:
-        process = subprocess.Popen([sys.executable, '-', '--system-site-packages', DEST], stdin=subprocess.PIPE)
+        process = subprocess.Popen([sys.executable, '-', '--system-site-packages', here], stdin=subprocess.PIPE)
         stdout, stderr = process.communicate(input=urllib2.urlopen(VIRTUALENV).read())
 
-    # create a src directory
-    src = os.path.join(DEST, 'src')
-    os.mkdir(src)
-
-    # get talos
-    hg = which('hg')
-    if hg:
-        call([hg, 'clone', REPO], cwd=src)
-        talosdir = os.path.join(src, 'talos')
-    else:
-        buffer = StringIO()
-        buffer.write(urllib2.urlopen(ZIPFILE).read())
-        zip = zipfile.ZipFile(buffer)
-        zip.extractall(path=src)
-        # the archive directory gets labeled as e.g. talos-214df9ee2ea7
-        # we need to figure out the prefix
-        member = zip.namelist()[0]
-        regex = re.compile(r'(talos-[' + string.hexdigits + r']+).*')
-        match = regex.match(member)
-        assert match
-        talosdir = os.path.join(src, match.groups()[0])
-
-    # find the correct python
+    # find the virtualenv's python
     for i in ('bin', 'Scripts'):
-        bindir = os.path.join(DEST, i)
+        bindir = os.path.join(here, i)
         if os.path.exists(bindir):
             break
     else:
@@ -88,7 +83,10 @@ def main(args=sys.argv[1:]):
         raise AssertionError('virtualenv python not found')
 
     # install talos into the virtualenv
-    call([os.path.abspath(virtualenv_python), 'setup.py', 'develop'], cwd=talosdir)
+    call([os.path.abspath(virtualenv_python), 'setup.py', 'develop'], cwd=here)
+
+    # get pageloader
+    get_pageloader(os.path.join(page_load_test, 'pageloader.xpi'))
 
 if __name__ == '__main__':
     main()
