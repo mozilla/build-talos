37c55f0eee290bb31f3a216aa801aee857831d6f
Jeff Hammel <jhammel@mozilla.com>
2012-08-23 23:05:35 -0700

    Bug 784863 - shlex error in checking for processes in utils.py;r=jmaher

--- talos/utils.py
+++ talos/utils.py
@@ -1,40 +1,8 @@
-# ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is standalone Firefox performance tests.
-#
-# The Initial Developer of the Original Code is The Mozilla Corporation.
-# Portions created by the Initial Developer are Copyright (C) 2007
-# the Initial Developer. All Rights Reserved.
-#
-# Contributor(s):
-#   Zach Lipton <zach@zachlipton.com>
-#
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the GPL or the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
-# ***** END LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-"""Utility functions"""
+"""Utility functions for Talos"""
 
 import os
 import shlex
@@ -219,14 +187,30 @@ def running_processes(name, psarg='axwww', defunct=False):
       print >> sys.stderr, "ps %s output:" % psarg
       print >> sys.stderr, _ps_output
       raise talosError("command not found in %s" % process)
-    command = shlex.split(command)
+
+    if name not in command:
+      # filter out commands where the name doesn't occur
+      continue
+
+    try:
+      command = shlex.split(command)
+    except ValueError:
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=784863
+      # shlex error in checking for processes in utils.py
+      print command
+      raise
+
     if command[-1] == '<defunct>':
+      # ignore defunct processes
       command = command[:-1]
       if not command or not defunct:
         continue
+
     if 'STAT' in process and not defunct:
       if process['STAT'] == 'Z+':
+        # ignore zombie processes
         continue
+
     prog = command[0]
     if prog.startswith('(') and prog.endswith(')'):
       prog = prog[1:-1]
