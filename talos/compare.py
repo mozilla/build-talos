68534f8187fdddf3590f454070870b330320133b
Joel Maher <jmaher@mozilla.com>
2013-05-07 14:22:40 -0400

    Bug 864430 - update compare.py to pull xperf data from datazilla. r=jhammel
    --HG--
    rename : compare.py => talos/compare.py

--- talos/compare.py
+++ talos/compare.py
@@ -6,16 +6,22 @@ import json, urllib, httplib
 import datetime, time
 from optparse import OptionParser
 import sys
+import os
+import results
 
 SERVER = 'graphs.mozilla.org'
 selector = '/api/test/runs'
 debug = 1
 
 branch_map = {}
-branch_map['Try'] = {'pgo': 23, 'nonpgo': 113}
-branch_map['Firefox'] = {'pgo': 1, 'nonpgo': 94}
-branch_map['Inbound'] = {'pgo': 63, 'nonpgo': 131}
-branch_map['Cedar'] = {'pgo': 26, 'nonpgo': 26}
+branch_map['Try']     = {'pgo':    {'id': 23, 'name': 'Try'}, 
+                         'nonpgo': {'id': 113, 'name': 'Try'}}
+branch_map['Firefox'] = {'pgo':    {'id': 1,  'name': 'Firefox'}, 
+                         'nonpgo': {'id': 94, 'name': 'Firefox-Non-PGO'}}
+branch_map['Inbound'] = {'pgo':    {'id': 63, 'name': 'Mozilla-Inbound'}, 
+                         'nonpgo': {'id': 131, 'name': 'Mozilla-Inbound-Non-PGO'}}
+branch_map['Cedar']   = {'pgo':    {'id': 26, 'name': 'Cedar'}, 
+                         'nonpgo': {'id': 26, 'name': 'Cedar'}}
 branches = ['Try', 'Firefox', 'Inbound', 'Cedar']
 
 # TODO: pull test names and reverse_tests from test.py in the future
@@ -37,8 +43,8 @@ test_map['tpaint'] = {'id': 82, 'tbplname': 'tpaint'}
 test_map['tsvgr'] = {'id': 224, 'tbplname': 'tsvgr'}
 test_map['tsvgr_opacity'] = {'id': 225, 'tbplname': 'tsvgr_opacity'}
 test_map['tresize'] = {'id': 254, 'tbplname': 'tresize'}
-#tests['tp5r_paint'] = 139
 test_map['tp5n'] = {'id': 206, 'tbplname': 'tp5n_paint'}
+test_map['tp5o'] = {'id': 255, 'tbplname': 'tp5o_paint'}
 tests = ['tdhtmlr', 'tresize', 'tdhtmlr_nochrome', 'tspaint_places_med', 'tspaint_places_max', 'kraken', 'v8', 'dromaeo_css', 'dromaeo_dom', 'tscrollr', 'a11yr', 'ts_paint', 'tpaint', 'tsvgr', 'tsvgr_opacity', 'tp5n']
 
 reverse_tests = ['dromaeo_css', 'dromaeo_dom']
@@ -47,12 +53,14 @@ platform_map = {}
 platform_map['Linux'] = 14
 platform_map['Linux64'] = 15
 platform_map['Win'] = 12
+platform_map['Win8'] = 31
 platform_map['WinXP'] = 1
 platform_map['Win64'] = 19
 platform_map['OSX10.7'] = 22
 platform_map['OSX64'] = 21 #10.6
 platform_map['OSX'] = 13 #10.5.8
-platforms = ['Linux', 'Linux64', 'Win', 'WinXP', 'OSX10.7', 'OSX64', 'OSX'] #'Win64' doesn't have talos results
+platform_map['OSX10.8'] = 24
+platforms = ['Linux', 'Linux64', 'Win', 'WinXP', 'Win8', 'OSX10.7', 'OSX64', 'OSX10.8'] #'Win64' doesn't have talos results
 
 def getGraphData(testid, branchid, platformid):
     body = {"id": testid, "branchid": branchid, "platformid": platformid}
@@ -75,6 +83,136 @@ def getGraphData(testid, branchid, platformid):
     if data['stat'] == 'fail':
         return None
     return data
+
+# TODO: consider moving this to mozinfo or datazilla_client
+def getDatazillaPlatform(os, platform, osversion, product):
+    platform = None
+    if product == 'Fennec':
+        platform = 'Tegra'
+
+    if os == 'linux':
+        if platform == 'x86_64':
+            platform = 'Linux64'
+        else:
+            platform = 'Linux'
+    elif os == 'win':
+        if osversion == '6.1.7600':
+            platform = 'Win'
+        elif osversion == '6.2.9200':
+            platform = 'Win8'
+        else:
+            platform = 'WinXP'
+    elif os == 'mac':
+        if osversion.startswith('OS X 10.6'):
+            platform = 'OSX64'
+        elif osversion.startswith('OS X 10.7'):
+            platform = 'OSX10.7'
+        elif osversion.startswith('OS X 10.8'):
+            platform = 'OSX10.8'
+    return platform
+
+
+#TODO: move this to datazilla_client.
+def getDatazillaCSET(revision, branchid):
+    testdata = {}
+    pgodata = {}
+    xperfdata = {}
+    cached = "%s.json" % revision
+    if os.path.exists(cached):
+        response = open(cahced, 'r')
+    else:
+        conn = httplib.HTTPSConnection('datazilla.mozilla.org')
+        cset = "/talos/testdata/raw/%s/%s" % (branchid, revision)
+        conn.request("GET", cset)
+        response = conn.getresponse()
+
+    data = response.read()
+    response.close()
+    cdata = json.loads(data)
+
+    for testrun in cdata:
+        values = []
+        platform = getDatazillaPlatform(testrun['test_machine']['os'], 
+                                        testrun['test_machine']['platform'], 
+                                        testrun['test_machine']['osversion'],
+                                        testrun['test_build']['name'])
+
+        if platform not in testdata:
+            testdata[platform] = {}
+
+        if platform not in pgodata:
+            pgodata[platform] = {}
+
+        pgo = True
+        if 'Non-PGO' in testrun['test_build']['branch']:
+            pgo = False
+
+        suite = testrun['testrun']['suite']
+        extension = ''
+        if not testrun['testrun']['options']['tpchrome']:
+            extension = "%s_nochrome" % extension
+
+        # these test names are reported without _paint
+        if 'svg' not in suite and \
+           'kraken' not in suite and \
+           'sunspider' not in suite and \
+           'dromaeo' not in suite and \
+           testrun['testrun']['options']['tpmozafterpaint']:
+            if 'paint' not in suite: # handle tspaint_places_generated_*
+                extension = "%s_paint" % extension
+
+        suite = "%s%s" % (suite, extension)
+        isRemote = testrun['test_build']['name'] == 'Fennec'
+
+        # This is a hack that means we are running xperf since tp5n was replaced by tp5o
+        if platform == "Win" and suite == "tp5n_paint":
+            xperfdata = testrun['results_xperf']
+
+        for page in testrun['results']:
+
+            index = 0
+            if len(testrun['results'][page]) > 0:
+                index = 0
+
+            vals = sorted(testrun['results'][page][index:])[:-1]
+            
+            if len(vals) == 0:
+                vals = [0]
+            values.append(float(sum(vals))/len(vals))
+
+        # ignore max
+        if len(values) >= 2:
+            values = sorted(values)[:-1]
+        if len(values) == 0:
+            values = [0]
+
+        # mean
+        if pgo:
+            pgodata[platform][suite] = float(sum(values)/len(values))
+            if debug > 1:
+                print "%s: %s: %s (PGO)" % (platform, suite, pgodata[platform][suite])
+        else:
+            testdata[platform][suite] = float(sum(values)/len(values))
+            if debug > 1:
+                print "%s: %s: %s" % (platform, suite, testdata[platform][suite])
+
+    return testdata, pgodata, xperfdata
+
+def getDatazillaData(branchid):
+    # TODO: resolve date, currently days_ago=7, 
+
+    # https://datazilla.mozilla.org/refdata/pushlog/list/?days_ago=7&branches=Mozilla-Inbound
+    conn = httplib.HTTPSConnection('datazilla.mozilla.org')
+    cset = "/refdata/pushlog/list/?days_ago=14&branches=%s" % branchid
+    conn.request("GET", cset)
+    response = conn.getresponse()
+    data = response.read()
+    jdata = json.loads(data)
+    alldata = {}
+
+    for item in jdata:
+        alldata[jdata[item]['revisions'][0]] = getDatazillaCSET(jdata[item]['revisions'][0], branchid)
+    return alldata
 
 def parseGraphResultsByDate(data, start, end):
     low = sys.maxint
@@ -109,17 +247,29 @@ def parseGraphResultsByChangeset(data, changeset):
     return {'low': low, 'high': high, 'count': count}
 
 
-def compareResults(revision, branch, masterbranch, startdate, enddate, platforms, tests, printurl):
+def compareResults(revision, branch, masterbranch, startdate, enddate, platforms, tests, printurl, dzdata, pgodzdata):
     for p in platforms:
         print "%s:" % (p)
         for t in tests:
-            test_bid = branch_map[branch]['nonpgo']
-            if p.startswith('OSX'):
-                test_bid = branch_map[branch]['pgo']
+            dzval = "N/A"
+            if dzdata:
+                if p in dzdata:
+                    if test_map[t]['tbplname'] in dzdata[p]:
+                        dzval = dzdata[p][test_map[t]['tbplname']]
 
-            bid = branch_map[masterbranch]['nonpgo']
+            pgodzval = "N/A"
+            if pgodzdata:
+                if p in pgodzdata:
+                    if test_map[t]['tbplname'] in pgodzdata[p]:
+                        pgodzval = pgodzdata[p][test_map[t]['tbplname']]
+
+            test_bid = branch_map[branch].nonpgo.id
             if p.startswith('OSX'):
-                bid = branch_map[masterbranch]['pgo']
+                test_bid = branch_map[branch]['pgo']['id']
+
+            bid = branch_map[masterbranch]['nonpgo']['id']
+            if p.startswith('OSX'):
+                bid = branch_map[masterbranch]['pgo']['id']
         
             data = getGraphData(test_map[t]['id'], bid, platform_map[p])
             testdata = getGraphData(test_map[t]['id'], test_bid, platform_map[p])
@@ -144,7 +294,7 @@ def compareResults(revision, branch, masterbranch, startdate, enddate, platforms
                     url = ""
                     if printurl:
                         url = shorten("http://graphs.mozilla.org/graph.html#tests=[[%s,%s,%s]]" % (test_map[t]['id'], bid, platform_map[p]))
-                    print "    %s%s: %s -> %s; %s.  %s" % (status, t, results['low'], results['high'], test['low'], url)
+                    print "    %s%s: %s -> %s; %s.  %s [%s] [PGO: %s]" % (status, t, results['low'], results['high'], test['low'], url, dzval, pgodzval)
             else:
                 print "    %s: No data for platform" % t
 
@@ -188,6 +338,16 @@ class CompareOptions(OptionParser):
                     default = False,
                     help = "Print a url that can link to the data in graph server")
 
+    self.add_option("--pgo",
+                    action = "store_true", dest = "pgo",
+                    default = False,
+                    help = "Use PGO Branch if available")
+
+    self.add_option("--xperf",
+                    action = "store_true", dest = "xperf",
+                    default = False,
+                    help = "Print xperf information")
+
 def main():
     global platforms, tests
     parser = CompareOptions()
@@ -208,14 +368,18 @@ def main():
     masterbranch = 'Firefox'
     if options.masterbranch:
         if options.masterbranch in branches:
-            masterbranch = options.masterbranch
+            masterbranch = branch_map[options.masterbranch]['nonpgo']['name']
+            if options.pgo:
+                masterbranch = branch_map[options.masterbranch]['pgo']['name']
         else:
             parser.error("ERROR: the masterbranch '%s' you specified does not exist in '%s'" % (options.masterbranch, branches))
 
     branch = 'Try'
     if options.branch:
         if options.branch in branches:
-            branch = options.branch
+            branch = branch_map[options.branch]['nonpgo']['name']
+            if options.pgo:
+                branch = branch_map[options.branch]['pgo']['name']
         else:
             parser.error("ERROR: the branch '%s' you specified does not exist in '%s'" % (options.branch, branches))
 
@@ -229,7 +393,14 @@ def main():
     startdate = int(time.mktime((datetime.datetime.now() - datetime.timedelta(days=(options.skipdays+14))).timetuple()))
     enddate = int(time.mktime((datetime.datetime.now() - datetime.timedelta(days=options.skipdays)).timetuple()))
 
-    compareResults(options.revision, branch, masterbranch, startdate, enddate, platforms, tests, options.printurl)
+    #TODO: We need to ensure we have full coverage of the pushlog before we can do this.  
+#    alldata = getDatazillaData(options.branch)
+#    datazilla, pgodatazilla, xperfdata = alldata[options.revision]
+    datazilla, pgodatazilla, xperfdata = getDatazillaCSET(options.revision, branch)
+    if options.xperf:
+        print xperfdata
+    else:
+        compareResults(options.revision, branch, masterbranch, startdate, enddate, platforms, tests, options.printurl, datazilla, pgodatazilla)
 
 def shorten(url):
     headers = {'content-type':'application/json'}
