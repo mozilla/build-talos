8027c9da5892e48eca673fc75e0a29a8368b879b
Avi Halachmi <avihpit@yahoo.com>
2013-07-09 18:27:39 +0300

    Bug 849558: add MPL clause. r=jmaher

--- talos/scripts/talos-debug.js
+++ talos/scripts/talos-debug.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /******************************************************************
  * window.talosDebug provides some statistical functions
  * (sum, average, median, stddev) and a tpRecordTime method which
