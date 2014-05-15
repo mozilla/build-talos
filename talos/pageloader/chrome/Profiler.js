56e30a8133598043215e20ffec24b23e1bad7f10
Avi Halachmi <avihpit@yahoo.com>
2014-05-15 17:18:43 +0300

    Bug 1010473: Talos: Fix Profiler.js for stand-alone cases (TART/tscroll). r=jmaher

--- talos/pageloader/chrome/Profiler.js
+++ talos/pageloader/chrome/Profiler.js
@@ -1,8 +1,25 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// - NOTE: This file is duplicated verbatim at:
+//         - talos/scripts/Profiler.js
+//         - talos/pageloader/chrome/Profiler.js
+//         - talos/page_load_test/tart/addon/content/Profiler.js
+//
+//  - Please keep these copies in sync.
+//  - Please make sure your changes apply cleanly to all use cases.
+
 // Finer grained profiler control
 //
 // Use this object to pause and resume the profiler so that it only profiles the
 // relevant parts of our tests.
-var Profiler;
+var Profiler = { // Initialize as placeholder, replace with actual if available.
+  resume: function() {},
+  pause:  function() {},
+  mark:   function() {}
+};
+
 (function(){
   var _profiler;
   var test_name = document.location.pathname;
@@ -10,7 +27,7 @@ var Profiler;
   try {
     netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
     _profiler = Components.classes["@mozilla.org/tools/profiler;1"].getService(Components.interfaces.nsIProfiler);
-  } catch (ex) { dumpLog(ex + "\n"); }
+  } catch (ex) { (typeof(dumpLog) == "undefined" ? dump : dumpLog)(ex + "\n"); }
 
   Profiler = {
     resume: function Profiler__resume (name) {
