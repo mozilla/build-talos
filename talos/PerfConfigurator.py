7f62f56056680ecac4aec4f8a2c4f231db2f7d8d
William Lachance <wlachance@mozilla.com>
2014-10-20 18:15:21 -0400

    Bug 1078250 - Enable CART/TART on e10s;r=jmaher

--- talos/PerfConfigurator.py
+++ talos/PerfConfigurator.py
@@ -563,7 +563,7 @@ the highest value.
             activeTests = activeTests.split(':')
         # temporary hack for now until we have e10s running on all tests; please remove if you are running locally
         if self.config.get('e10s'):
-            for testname in ['tresize', 'cart', 'tart', 'tp5o_scroll']:
+            for testname in ['tresize', 'tp5o_scroll']:
                 if testname in activeTests:
                     print "%s is unsupported on e10s, removing from list of " \
                         "tests to run" % testname
