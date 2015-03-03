75fd8f86e540f3b834329c7075448e5c262611a2
Alessio Placitelli <alessio.placitelli@gmail.com>
2015-03-03 08:50:08 -0500

    Bug 1138823 - Set browser.selfsupport.url to a dummy URL for talos tests. r=jmaher

--- talos/PerfConfigurator.py
+++ talos/PerfConfigurator.py
@@ -291,6 +291,7 @@ the highest value.
         'browser.safebrowsing.malware.enabled': False,
         'browser.search.isUS': True,
         'browser.search.countryCode': 'US',
+        'browser.selfsupport.url': 'http://127.0.0.1/selfsupport-dummy/',
         'extensions.update.url': 'http://127.0.0.1/extensions-dummy/updateURL',
         'extensions.update.background.url': 'http://127.0.0.1/extensions-dummy/updateBackgroundURL',
         'extensions.blocklist.enabled': False,
