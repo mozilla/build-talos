a5bd9735363d5b7fb001f2403cbd7973397f657d
Markus Stange <mstange@themasta.com>
2015-07-27 13:40:15 -0400

    Bug 1188013 - Use a different symbolication server URL in order to identify symbolication requests coming from Talos profiling. r=vladan, r=jmaher

--- talos/ttest.py
+++ talos/ttest.py
@@ -525,7 +525,7 @@ class TTest(object):
                         "enableTracing": 0,
                         # Fallback server if symbol is not found locally
                         "remoteSymbolServer":
-                            "http://symbolapi.mozilla.org:80/",
+                            "http://symbolapi.mozilla.org:80/talos/",
                         # Maximum number of symbol files to keep in memory
                         "maxCacheEntries": 2000000,
                         # Frequency of checking for recent symbols to
